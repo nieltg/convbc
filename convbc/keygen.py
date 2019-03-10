25a84457c1df8d8bf0fd0c18d3b8c82d788a9cd7
Daniel <nieltansah@gmail.com>
2019-03-10 18:00:27 +0700

    keygen: KeyGenerator's seed can't be odd lengthed ndarray

--- convbc/keygen.py
+++ convbc/keygen.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def KeyGenerator(seed):
-    if seed.ndim != 1 or seed.dtype != np.uint8:
+    len_dim0, = seed.shape
+    if len_dim0 % 2 != 0 or seed.dtype != np.uint8:
         raise Exception()
     return object()
--- convbc/tests/test_keygen.py
+++ convbc/tests/test_keygen.py
@@ -8,6 +8,11 @@ def test_key_generator_without_seed():
     with pytest.raises(TypeError):
         keygen.KeyGenerator()
 
+def test_key_generator_with_odd_lengthed_ndarray_as_seed():
+    seed = np.arange(1, dtype=np.uint8)
+    with pytest.raises(Exception):
+        keygen.KeyGenerator(seed)
+
 def test_key_generator_with_multidimensional_ndarray_as_seed():
     seed = np.arange(2, dtype=np.uint8).reshape(-1, 1)
     with pytest.raises(Exception):
