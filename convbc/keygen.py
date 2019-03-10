17c5360b807e64a49fe79df29a7a7437274edaea
Daniel <nieltansah@gmail.com>
2019-03-10 17:46:39 +0700

    keygen: KeyGenerator's seed can't be non-uint8 dtyped ndarray

--- convbc/keygen.py
+++ convbc/keygen.py
@@ -1,4 +1,7 @@
+import numpy as np
+
+
 def KeyGenerator(seed):
-    if seed.ndim != 1:
+    if seed.ndim != 1 or seed.dtype != np.uint8:
         raise Exception()
     return object()
--- convbc/tests/test_keygen.py
+++ convbc/tests/test_keygen.py
@@ -13,6 +13,11 @@ def test_key_generator_with_multidimensional_ndarray_as_seed():
     with pytest.raises(Exception):
         keygen.KeyGenerator(seed)
 
+def test_key_generator_with_non_uint8_dtyped_ndarray_as_seed():
+    seed = np.arange(2, dtype=np.float)
+    with pytest.raises(Exception):
+        keygen.KeyGenerator(seed)
+
 def test_key_generator_with_seed():
     seed = np.arange(2, dtype=np.uint8)
     assert keygen.KeyGenerator(seed) is not None
