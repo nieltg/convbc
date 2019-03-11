e7eb7fb12a10bd818555410fe67ca8fce6ac79fd
Daniel <nieltansah@gmail.com>
2019-03-11 15:12:11 +0700

    keygen: Create expand_key which can't be called without key

---
+++ convbc/keygen.py
@@ -0,0 +1,2 @@
+def expand_key(key):
+    pass
--- convbc/tests/test_keygen.py
+++ convbc/tests/test_keygen.py
@@ -1,1 +1,8 @@
+import pytest
+
 from .. import keygen
+
+
+def test_expand_key_without_seed():
+    with pytest.raises(TypeError):
+        keygen.expand_key()
