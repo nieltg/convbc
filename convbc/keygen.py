b911134ae3703c3e783566e65c4184ed18857f30
Daniel <nieltansah@gmail.com>
2019-03-11 15:04:54 +0700

    keygen: Reimpl. expand_key because of changes in contract
    Old contract:
      expand_key(seed: 1d array, limit) -> iterator of 1d array
    New contract:
      expand_key(key: x-dim array, n) -> (x+1)-dim array

--- convbc/keygen.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def expand_key(seed, length=24):
-    len_dim0, = seed.shape
-    if len_dim0 % 2 != 0 or seed.dtype != np.uint8:
-        raise Exception()
-    return [None] * length
