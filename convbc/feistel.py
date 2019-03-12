77358eb19a1b8e3289f25d93b7d50e197f30a52a
Daniel <nieltansah@gmail.com>
2019-03-13 00:13:29 +0700

    feistel: Apply yapf, inverse_feistel

--- convbc/feistel.py
+++ convbc/feistel.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 def split_plain_block(plain_block):
     n = len(plain_block)
-    return plain_block[:n//2], plain_block[-n//2:]
+    return plain_block[:n // 2], plain_block[-n // 2:]
 
 
 def combine_plain_block(plain_L, plain_R):
@@ -17,4 +18,15 @@ def feistel_network(func, plain_block, round_keys):
         plain_R = plain_L ^ func(round_keys[i], plain_R)
         plain_L = tmp
     cipher_block = combine_plain_block(plain_R, plain_L)
+    return cipher_block
+
+
+def inverse_feistel_network(func, plain_block, round_keys):
+    n = round_keys.shape[0]
+    plain_R, plain_L = split_plain_block(plain_block)
+    for i in range(n):
+        tmp = plain_L
+        plain_L = plain_R ^ func(round_keys[n - i - 1], plain_L)
+        plain_R = tmp
+    cipher_block = combine_plain_block(plain_L, plain_R)
     return cipher_block
